480153d43581bf108c8bf33c44bfa19649f3ff3e
Osman <osmanhajr32@gmail.com>
2020-10-29 15:27:06 +0000

    Commit changes

--- server.js
+++ server.js
@@ -4,9 +4,10 @@
 //load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
+app.use(cors())
 //load the quotes JSON
-const quotes = require("./quotes.json");
+let quotes = require("./quotes.json");
 
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
@@ -18,14 +19,35 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 app.get("/quotes", function (request, response) {
-  const allQuotes = quotes.map((elements) => {
-    return `quote: ${elements.quote} author: ${elements.author}`
-  })
-  response.send(allQuotes);
+  const allQuotes = quotes.map((q) => 
+    (
+      `{"quote": "${q.quote}", "author": "${q.author}"}`
+    ))
+  response.send(`[${allQuotes}]`);
 });
 app.get("/quotes/random", function (request, response) {
-  response.send(pickFromArray(quotes));
+  const randomQuote = pickFromArray(quotes.map(q=>(q.quote)))
+  const randomAuthor = pickFromArray(quotes.map(q=>(q.author)))
+  response.send(`{"quote": "${randomQuote}", "author": "${randomAuthor}" }`);
 });
+app.get("/quotes/search", function(req, res){
+  let term = req.query.term;
+  res.send(`The term you're searching for is: ${term.toLocaleLowerCase()}`)
+})
+app.get('/search/filtered/quotes', function (req, res){
+  let term = req.query.term; 
+  let filteredQuotes = (quotes, term)=>{
+    return quotes.filter((q) => (q.quote.toLowerCase().includes(term)));
+  }
+  res.send(filteredQuotes(quotes, term));
+})
+app.get('/search/filtered/authors', function (req, res){
+  let name = req.query.name; 
+  let filteredAuthors = (quotes)=>{
+    return quotes.filter((q) => (q.author.toLowerCase().includes(name)));
+  }
+  res.send(filteredAuthors(quotes));
+})
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
